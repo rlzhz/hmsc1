a1b0af7194f02a23b4c066780914ddfe6c9275af
赵慧喆 <925774232@qq.com>
2020-04-27 16:03:25 +0800

    user 修改密码完成，整个user模块结束

--- web/controllers/account/Account.py
+++ web/controllers/account/Account.py
@@ -1,12 +1,17 @@
 from flask import Blueprint
 
 from common.libs.Helper import ops_render
+from common.models.User import User
+
 
 router_account = Blueprint("account_page",__name__)
 
 @router_account.route('/index',)
 def index():
-    return ops_render('account/index.html')
+    resp_data ={}
+    list = User.query.all()
+    resp_data['list'] = list
+    return ops_render('account/index.html',resp_data)
 
 @router_account.route('/info',)
 def info():
