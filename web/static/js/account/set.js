cc4292af3aff4ff9c2057344317bf50cad929c2f
赵慧喆 <925774232@qq.com>
2020-04-28 15:12:20 +0800

    更新商品管理模板

--- web/static/js/account/set.js
+++ web/static/js/account/set.js
@@ -1,62 +1,89 @@
 ;
 var account_set_ops = {
-    init:function () {
-
-        this.eventBind()
+    init:function(){
+        this.eventBind();
     },
-    eventBind:function () {
-        $('.wrap_account_set .save').click(function () {
-            console.log('set')
-            var btn_target = $(this)
-            if (btn_target.hasClass('disabled')){
-                alert("正在处理")
-                return ;
+    eventBind:function(){
+        $(".wrap_account_set .save").click(function(){
+            var btn_target = $(this);
+            if( btn_target.hasClass("disabled") ){
+                common_ops.alert("正在处理!!请不要重复提交~~");
+                return;
             }
-            var nickname_value = $('.wrap_account_set input[name=nickname]').val();
-            var email_value = $('.wrap_account_set input[name=email]').val();
-            var mobile_value = $('.wrap_account_set input[name=mobile]').val();
-            var login_name_value = $('.wrap_account_set input[name=login_name]').val();
-            var login_pwd_value = $('.wrap_account_set input[name=login_pwd]').val();
-            if (!nickname_value || nickname_value.length < 2) {
-                alert('请输入规范的昵称')
-                return false
+
+            var nickname_target = $(".wrap_account_set input[name=nickname]");
+            var nickname = nickname_target.val();
+
+            var mobile_target = $(".wrap_account_set input[name=mobile]");
+            var mobile = mobile_target.val();
+
+            var email_target = $(".wrap_account_set input[name=email]");
+            var email = email_target.val();
+
+            var login_name_target = $(".wrap_account_set input[name=login_name]");
+            var login_name = login_name_target.val();
+
+            var login_pwd_target = $(".wrap_account_set input[name=login_pwd]");
+            var login_pwd = login_pwd_target.val();
+
+            if( nickname.length < 1 ){
+                common_ops.tip( "请输入符合规范的姓名~~",nickname_target );
+                return false;
             }
-            if (!email_value || email_value.length < 2) {
-                alert('请输入规范的昵称')
-                return false
+
+            if( mobile.length < 1 ){
+                common_ops.tip( "请输入符合规范的手机号码~~",mobile_target );
+                return false;
             }
-            if (!mobile_value || mobile_value.length < 2) {
-                alert('请输入规范的昵称')
-                return false
+
+            if(  email.length < 1 ){
+                common_ops.tip( "请输入符合规范的邮箱~~",email_target );
+                return false;
             }
-            if (!login_name_value || login_name_value.length < 2) {
-                alert('请输入规范的昵称')
-                return false
+
+            if( login_name.length < 1 ){
+                common_ops.tip( "请输入符合规范的登录用户名~~",login_name_target );
+                return false;
             }
-            if (!login_pwd_value || login_pwd_value.length < 2) {
-                alert('请输入规范的昵称')
-                return false
+
+            if( login_pwd.length < 6 ){
+                common_ops.tip( "请输入符合规范的登录密码~~",login_pwd_target );
+                return false;
             }
-            btn_target.addClass('disabled');
+
+            btn_target.addClass("disabled");
+
+            var data = {
+                nickname: nickname,
+                mobile: mobile,
+                email: email,
+                login_name:login_name,
+                login_pwd:login_pwd,
+                id:$(".wrap_account_set input[name=id]").val()
+            };
+
             $.ajax({
-                url:common_ops.buildUrl("/account/set"),
-                type:"POST",
-                data:{'nickname':nickname_value,'email':email_value, 'login_pwd':login_pwd_value, 'login_name':login_name_value,'mobile':mobile_value},
+                url:common_ops.buildUrl( "/account/set" ),
+                type:'POST',
+                data:data,
                 dataType:'json',
-                success:function(resp){
-
-                    console.log(resp)
-                    alert(resp.msg)
-                    btn_target.removeClass('disabled');
-
-                },
-                error:function(error){
-                    console.log(error)
+                success:function( res ){
+                    btn_target.removeClass("disabled");
+                    var callback = null;
+                    if( res.code == 200 ){
+                        callback = function(){
+                            window.location.href = common_ops.buildUrl("/account/index");
+                        }
+                    }
+                    common_ops.alert( res.msg,callback );
                 }
-            })
-        })
+            });
+
+
+        });
     }
-}
-$(document).ready(function () {
-    account_set_ops.init()
-})
+};
+
+$(document).ready( function(){
+    account_set_ops.init();
+} );
