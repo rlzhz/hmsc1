8c932fddd26c2b736b196db323505f86e52d9a98
赵慧喆 <925774232@qq.com>
2020-04-28 14:50:42 +0800

    筛选

--- web/controllers/account/Account.py
+++ web/controllers/account/Account.py
@@ -1,53 +1,66 @@
-from flask import Blueprint,request,redirect, jsonify
+from flask import Blueprint, request, redirect, jsonify
+
 from application import db
-from common.libs.Helper import ops_render
+from common.libs.Helper import ops_render, getCurrentDate
 from common.libs.UrlManager import UrlManager
-from common.models.User import User
-from common.libs.Helper import getCurrentDate
 from common.libs.user.UserService import UserService
+from common.models.User import User
+
+router_account = Blueprint("account_page", __name__)
 
 
-router_account = Blueprint("account_page",__name__)
-
-@router_account.route('/index',)
+@router_account.route('/index')
 def index():
-    resp_data ={}
-    list = User.query.all()
-    resp_data['list'] = list
-    return ops_render('account/index.html',resp_data)
+    resp_data = {}
+    req = request.values
+    query = User.query
+    if 'status' in req and int(req['status']) > -1:
+        query = query.filter(User.status == int(req['status']))
 
-@router_account.route('/info',)
+    list = query.all()
+    resp_data['list'] = list
+    resp_data['status'] = {
+        "1": "正常",
+        "0": "已删除"
+    }
+    return ops_render('account/index.html', resp_data)
+
+
+@router_account.route('/info')
 def info():
     resp_data = {}
     req = request.args
-    uid = int(req.get('id',0))
-    reback_url = UrlManager.buildUrl('/account/index')
+    uid = int(req.get("id", 0))
+    reback_url = UrlManager.buildUrl("/account/index")
     if uid < 1:
         return redirect(reback_url)
+
     info = User.query.filter_by(uid=uid).first()
     if not info:
         return redirect(reback_url)
-    resp_data['info'] = info
-    return ops_render('account/info.html',resp_data)
 
-@router_account.route('/set',methods=['GET','POST'])
+    resp_data['info'] = info
+    return ops_render('account/info.html', resp_data)
+
+
+@router_account.route('/set', methods=['GET', 'POST'])
 def set():
-    if request.method == 'GET':
+    if request.method == "GET":
         resp_data = {}
         req = request.args
-        uid = int(req.get('id',0))
+        uid = int(req.get("id", 0))
         info = None
         if uid:
             info = User.query.filter_by(uid=uid).first()
         resp_data['info'] = info
         return ops_render('account/set.html', resp_data)
-    # post 更新数据库
+    # POST  更新数据库
     resp = {
-        'code':200,
-        'msg':'操作已经成功',
-        'data':{}
+        'code': 200,
+        'msg': "操作成功",
+        'data': {}
     }
-    
+    # ajax 发送的数据
     req = request.values
     id = req['id'] if 'id' in req else 0
     nickname = req['nickname'] if 'nickname' in req else ''
@@ -58,31 +71,32 @@ def set():
 
     if nickname is None or len(nickname) < 1:
         resp['code'] = -1
-        resp['msg'] = '请输入符合规范的昵称'
+        resp['msg'] = "请输入符合规范的昵称"
         return jsonify(resp)
     if mobile is None or len(mobile) < 1:
         resp['code'] = -1
-        resp['msg'] = '请输入符合规范的昵称'
+        resp['msg'] = "请输入符合规范的手机号"
         return jsonify(resp)
     if email is None or len(email) < 1:
         resp['code'] = -1
-        resp['msg'] = '请输入符合规范的昵称'
+        resp['msg'] = "请输入符合规范的邮箱"
         return jsonify(resp)
     if login_name is None or len(login_name) < 1:
         resp['code'] = -1
-        resp['msg'] = '请输入符合规范的昵称'
+        resp['msg'] = "请输入符合规范的登录用户名"
         return jsonify(resp)
     if login_pwd is None or len(login_pwd) < 6:
         resp['code'] = -1
-        resp['msg'] = '请输入符合规范的昵称'
-        return jsonify(resp)
-    is_exsits = User.query.filter(User.login_name == login_name).first()
-    if is_exsits:
-        resp['code'] = -1
-        resp['msg'] = '该登录名已经存在,请更换'
+        resp['msg'] = "请输入符合规范的密码"
         return jsonify(resp)
 
-    user_info = User.query.filter_by(uid = id).first()
+    is_exsits = User.query.filter(User.login_name == login_name, User.uid != id).first()
+    if is_exsits:
+        resp['code'] = -1
+        resp['msg'] = "该登录名已经存在，请更换"
+        return jsonify(resp)
+
+    user_info = User.query.filter_by(uid=id).first()
     if user_info:
         model_user = user_info
     else:
@@ -94,9 +108,49 @@ def set():
     model_user.mobile = mobile
     model_user.email = email
     model_user.login_name = login_name
-
+    if user_info and user_info.uid == 1:
+        resp['code'] = -1
+        resp['msg'] = "该用户为Bruce，不允许修改"
+        return jsonify(resp)
+    model_user.login_pwd = UserService.generatePwd(login_pwd, model_user.login_salt)
     model_user.updated_time = getCurrentDate()
+
     db.session.add(model_user)
     db.session.commit()
+    return jsonify(resp)
 
+
+@router_account.route('removeOrRecover', methods=["GET", "POST"])
+def removeOrRecover():
+    resp = {
+        'code': -1,
+        'msg': '操作成功',
+        'data': {}
+    }
+    req = request.values
+    id = req['id'] if 'id' in req else 0
+    acts = req['acts'] if 'acts' in req else ''
+    if acts not in ['remove', 'recover']:
+        resp['code'] = -1
+        resp['msg'] = "操作有误"
+        return jsonify(resp)
+
+    if id:
+        user_info = User.query.filter_by(uid=id).first()
+        if not user_info:
+            resp['code'] = -1
+            resp['msg'] = "指定的账号不存在"
+            return jsonify(resp)
+        if user_info and user_info.uid == 1:
+            resp['code'] = -1
+            resp['msg'] = "该用户为Bruce，不能操作账号"
+            return jsonify(resp)
+        if acts == "remove":
+            user_info.status = 0
+        elif acts == "recover":
+            user_info.status = 1
+
+        user_info.updated_time = getCurrentDate()
+        db.session.add(user_info)
+        db.session.commit()
     return jsonify(resp)
